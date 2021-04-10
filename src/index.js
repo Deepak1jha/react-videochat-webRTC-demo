ed13e74cb69b84e76d642ea31e7cbd496975dd67
Deepak1jha <ddjha171@gmail.com>
2021-04-10 22:34:58 +0530

    ContestProvider Added

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 import App from './App';
+import {ContextProvider} from './SocketContext';
 
-ReactDOM.render(<App/>,document.getElementById('root'));
+ReactDOM.render(
+    <ContextProvider>
+        <App/>
+    </ContextProvider>, document.getElementById('root'));
